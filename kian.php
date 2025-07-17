96d1a02c86d31faa76118aeb24ccaadc04d69189
Naikhehe <kianilao94@gmail.com>
2025-07-17 08:15:01 +0800

    Add files via upload

---
+++ kian.php
@@ -0,0 +1,14 @@
+<?php
+echo "My name is Kian B. Ilao <br>";
+echo "I am 22 years old <br>";
+echo "My favorite subject is Computer Programming <br>";
+echo "My dream job is Web Developer <br>";
+echo "My motto in life: 'Seek Knowledge, Build a Future' <br>";
+echo "Here is my ASCII Art:<br><pre>";
+echo "  / \\__<br>";
+echo " (    @\\___<br>";
+echo " /         O<br>";
+echo "/   (_____/<br>";
+echo "/_____/     <br>";
+echo "</pre>";
+?>
